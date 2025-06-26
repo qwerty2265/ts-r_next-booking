635b70d45b2a73cab2eaae00dd6844436e222d84
MAXIM MUN <maksimmun007@gmail.com>
2025-06-26 15:05:21 +0500

    feat(layout): enable CSS layer support in AppRouterCacheProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body>
-        <AppRouterCacheProvider>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
          {children}
         </AppRouterCacheProvider>
       </body>
