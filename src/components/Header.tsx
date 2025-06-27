1e9e1c766c794297b15e98f80d085eb51f3b667f
MAXIM MUN <maksimmun007@gmail.com>
2025-06-27 15:19:47 +0500

    feat(header): implement session-based user display in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { Button, Typography } from "@mui/material";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Header() {
+  const {data: session} = useSession();
+
   return (
     <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
       <div>
@@ -8,14 +14,30 @@ export default function Header() {
           Расписание
         </Link>
       </div>
-      <div className="flex gap-4">
-        <Link href="/auth/login" className="text-xl font-semibold">
-          Логин
-        </Link>
-        <Link href="/auth/register" className="text-xl font-semibold">
-          Регистрация
-        </Link>
-      </div>
+
+      {session ? (
+        <div className="flex gap-4 items-center">
+          <Typography>
+            {`${session.user.name} ${session.user.email}`}
+          </Typography>
+          <Button
+            variant="outlined"
+            color="error"
+            onClick={() => signOut({ callbackUrl: '/' })}
+          >
+            Выйти
+          </Button>
+        </div>
+      ) : (
+        <div className="flex gap-4">
+          <Link href="/auth/login" className="text-xl font-medium hover:underline">
+            Логин
+          </Link>
+          <Link href="/auth/register" className="text-xl font-medium hover:underline">
+            Регистрация
+          </Link>
+        </div>
+      )}
     </header>
   )
 }
